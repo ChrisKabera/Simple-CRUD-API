5d0143ecd34dfaf9a976e3b7aab087b14f562367
Kabera Christian <102355157+ChrisKabera@users.noreply.github.com>
2024-10-29 15:06:31 +0200

    Update Employee.java

--- Recap/src/main/java/com/ck/Recap/Model/Employee.java
+++ Recap/src/main/java/com/ck/Recap/Model/Employee.java
@@ -1,11 +1,15 @@
 package com.ck.Recap.Model;
 
-
 import jakarta.persistence.*;
-import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name="Emp")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
 
     @Id
@@ -20,46 +24,4 @@ public class Employee {
 
     @Column(name = "Email")
     private String email;
-
-    public Employee() {
-    }
-
-    public Employee(String firstName, String lastName, String email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
-
